03977215d145501187a09f23ec4409752c0bbe0b
Victor Almeida <chapadin@gmail.com>
2022-06-03 08:20:12 -0300

    initial commit

---
+++ src/main/java/br/com/victor/crudspring/model/Course.java
@@ -0,0 +1,19 @@
+package br.com.victor.crudspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 200, nullable = false)
+    private String name;
+    @Column(length = 10, nullable = false)
+    private String category;
+}
---
+++ src/main/java/br/com/victor/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package br.com.victor.crudspring.repository;
+
+import br.com.victor.crudspring.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+
+}
